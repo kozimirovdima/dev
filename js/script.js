8e7a710c29f335c048c40f9b3d2679cd1b49abae
kozimirovdima <kozimirovdima23@gmail.com>
2024-04-04 21:12:15 +0300

    lesson 09

--- js/script.js
+++ js/script.js
@@ -3,48 +3,44 @@
 
 // задание 1
 // Получаем элемент h1 по тегу
-var titleElement = document.getElementsByTagName("h1")[0];
+const titleElement = document.getElementsByTagName("h1")[0];
 // Выводим текст заголовка в консоль для проверки
 console.log(titleElement.innerText);
 
 // задание 2
 // Получаем коллекцию элементов с классом "handler_btn"
-let buttons = document.getElementsByClassName("handler_btn");
-// Преобразуем коллекцию в массив для удобства работы
-let buttonsArray = Array.from(buttons);
-// Проходимся по массиву кнопок и выводим текст каждой кнопки
-buttonsArray.forEach(function (button) {
-  console.log(button.innerText);
-});
-
+const buttons = document.getElementsByClassName("handler_btn");
+const buttonStart = document.getElementsByClassName("handler_btn")[0];
+const buttonReset = document.getElementsByClassName("handler_btn")[1];
+console.log(buttonStart);
+console.log(buttonReset);
 // задание 3
 // Получаем кнопку "+"
-let addButton = document.querySelector(".screen-btn");
+const addButton = document.querySelector(".screen-btn");
 // Выводим текст кнопки в консоль для проверки
 console.log(addButton);
 
 // задание 4
 // Получаем элементы с классом "other-items" и "percent"
-let itemsWithPercent = document.querySelectorAll(".other-items.percent");
+const itemsWithPercent = document.querySelectorAll(".other-items.percent");
 // Получаем элементы с классом "other-items" и "number"
-let itemsWithNumber = document.querySelectorAll(".other-items.number");
+const itemsWithNumber = document.querySelectorAll(".other-items.number");
 
 // задание 5
 // Получаем input типа range через его родителя с классом "rollback"
-let rangeInput = document.querySelector(".rollback input[type='range']");
+const rangeInput = document.querySelector(".rollback input[type='range']");
 // Выводим полученный input в консоль для проверки
 console.log(rangeInput);
 
 // Задание 6
 // Получаем <span> с классом "range-value" через его родителя с классом "rollback"
-let rangeValueSpan = document.querySelector(".rollback .range-value");
+const rangeValueSpan = document.querySelector(".rollback .range-value");
 // Выводим полученный <span> в консоль для проверки
 console.log(rangeValueSpan);
 
 // Задание 7
 // Получаем коллекцию элементов с классом "total-input"
-let inputsCollection = document.getElementsByClassName("total-input");
-console.dir(inputsCollection);
+const inputsCollection = document.getElementsByClassName("total-input");
 console.log(inputsCollection[0]);
 console.log(inputsCollection[1]);
 console.log(inputsCollection[2]);
@@ -52,8 +48,7 @@ console.log(inputsCollection[3]);
 console.log(inputsCollection[4]);
 // задание 8
 // Изменяемая переменная, в которой будут храниться блоки с классом "screen"
-let screenBlocks = document.querySelectorAll(".screen");
+const screenBlocks = document.querySelectorAll(".screen");
 // Выводим полученные блоки в консоль для проверки
-console.dir(screenBlocks);
 console.log(screenBlocks[0]);
 // Конец 9 урока
